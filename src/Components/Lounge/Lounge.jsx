f20b0b989032cf4a030006a99280771e7e820992
Solomiya <solomiya04@gmail.com>
2023-09-04 19:42:02 +0300

    finished the section Lounge

--- src/Components/Lounge/Lounge.jsx
+++ src/Components/Lounge/Lounge.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import imageGrid from '../../assets/grid_2.png'
+
 
 const Lounge = () => {
   return (
-    <div>Lounge</div>
+    <div className='lounge container section'>
+      <div className="sectionContainer grid">
+        
+        <div className="imgDiv">
+          <img src={imageGrid}/>
+        </div>
+
+        <div className="textDiv">
+          <h2>Unccompanied Minor Lounge</h2>
+          <div className="grids grid">
+
+            <div className="singleGrid">
+              <span className='gridTitle'>
+                Help throught the airport
+              </span>
+              <p>You can also call airplanes from your phone and book a flight ticket to one of your favorite destinations</p>
+            </div>
+
+            <div className="singleGrid">
+              <span className='gridTitle'>
+                Help throught the airport
+              </span>
+              <p>You can also call airplanes from your phone and book a flight ticket to one of your favorite destinations</p>
+            </div>
+
+            <div className="singleGrid">
+              <span className='gridTitle'>
+                Care on the flight
+              </span>
+              <p>You can also call airplanes from your phone and book a flight ticket to one of your favorite destinations</p>
+            </div>
+
+            <div className="singleGrid">
+              <span className='gridTitle'>
+                Chauffeur-drive service
+              </span>
+              <p>You can also call airplanes from your phone and book a flight ticket to one of your favorite destinations</p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
